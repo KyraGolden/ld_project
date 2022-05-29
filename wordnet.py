82b3d691ce52a6e61c22f75c27ddb08257ea759d
KyraGolden <47739492+KyraGolden@users.noreply.github.com>
2022-05-30 00:30:37 +0200

    wordnet functions

---
+++ wordnet.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+# *-* coding: UTF-8 *-*
+# Authors: Jessica Roady & Kyra Goud
+
+
+import argparse
+import nltk
+from nltk.corpus import wordnet as wn
+"""
+***packages you will need to download***
+nltk.download('wordnet')
+nltk.download('omw-1.4')
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('maxent_ne_chunker')
+nltk.download('words')
+from nltk import word_tokenize, pos_tag, ne_chunk
+ # input for this script is the output fom the  remove duplicates
+"""
+
+def findWord(userInput, text):
+  """
+  still to do
+  """
+  if userInput in text:
+      return True
+  else:
+      return False
+
+def getRelationship(word1,word2):
+  compared1 = wn.synsets(word1)
+  compared2 = wn.synsets(word2)
+  for x in compared1:
+      for y in compared2:
+          score = x.wup_similarity(y)
+          score = score if score else 0
+          print(x, y, score)
+          return(x, y, score)
+
+def printDefinition(word1):
+  """
+  people should be able to choose which one it is then
+  """
+  print(word1.definition())
+  return(word1)
+
+def manuallyChooseDefinition():
+  """
+  method to choose noun
+  """
+  word1 = input("What word do you want? ")
+  listOfSynset = wn.synsets(word1)
+  print(wn.synsets(word1))
+  option = input(("What option is it? " ))
+  meaning = listOfSynset[int(option)]
+  #todo get babelfy meaning and get it to compare to description. match the best
+  return meaning
+
+def getLowestSynset(meaning1,meaning2):
+  """
+  method can only be used when word 1 and 2 are in format 'maria.n.01' with the method: manuallyChooseDefinition - this will later become non manual
+  """
+  print(meaning1.lowest_common_hypernyms(meaning2))
+  return (meaning1.lowest_common_hypernyms(meaning2))
+
+def main():
+  meaning1 = manuallyChooseDefinition()
+  meaning2 = manuallyChooseDefinition()
+  getLowestSynset(meaning1,meaning2)
+
+
+  #getRelationship(word1,word2)
+
+if __name__ == '__main__':
+    main()
